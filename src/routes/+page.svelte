8665fa1262f6e8db153457692d0f25b7d4d4e6d8
gmfmi <20098420+gmfmi@users.noreply.github.com>
2023-07-09 23:08:49 +0200

    feat: mobile friendly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,14 @@
 
 	import { game } from "./game-state";
 
-	// $: { console.log($game); }
+	$: {
+		console.log($game);
+	}
 
 	/** Whether or not the user has won */
 	$: won = $game.answers.at(-1) === "x".repeat($game.wordLength);
 	$: endRound = won || $game.answers.length === $game.guesses.length;
+	$: isLastRound = $game.wordId + 1 == $game.totalWords;
 
 	/** The index of the current guess */
 	$: currentIndex = won ? -1 : $game.answers.length;
@@ -97,123 +100,154 @@
 <main>
 	<img src={Logo} alt="" class="logo" />
 
-	<p class="word-id">
-		Thème: « {$game.theme} » | mot n°{$game.wordId + 1} sur {$game.totalWords}
-	</p>
+	<div class="info">
+		<p>Thème <strong>« {$game.theme} »</strong></p>
+		<p>Mot n°{$game.wordId + 1} sur {$game.totalWords}</p>
+	</div>
 
-	<form>
+	<div class="game">
 		<div class="grid-container">
-			<div
-				class="grid"
-				style="--row-length:{$game.wordLength};aspect-ratio:{$game.wordLength}/{$game
-					.guesses.length};"
-				class:playing={!won}
-			>
-				{#each Array.from(Array($game.guesses.length).keys()) as row (row)}
-					{@const current = row === currentIndex}
-					<h2 class="visually-hidden">Row {row + 1}</h2>
-					<div class="row" class:current>
-						{#each Array.from(Array($game.wordLength).keys()) as column (column)}
-							{@const answer = $game.answers[row]?.[column]}
-							{@const value = $game.guesses[row]?.[column] ?? ""}
-							{@const selected =
-								current && column === $game.guesses[row].length}
-							{@const exact = answer === "x"}
-							{@const close = answer === "c"}
-							{@const missing = answer === "_"}
-							<div
-								class="letter"
-								class:exact
-								class:close
-								class:missing
-								class:selected
-							>
-								{value}
-								<span class="visually-hidden">
-									{#if exact}
-										(correct)
-									{:else if close}
-										(present)
-									{:else if missing}
-										(absent)
-									{:else}
-										empty
-									{/if}
-								</span>
-								<input name="guess" disabled={!current} type="hidden" {value} />
-							</div>
-						{/each}
-					</div>
-				{/each}
-			</div>
-		</div>
-
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		<div class="end-round-panel">
-			{#if endRound}
-				<button
-					on:click={game.next}
-					style="border-color: {won ? 'green' : 'red'};"
+			<div class="grid-content">
+				<div
+					class="grid"
+					style="--row-length:{$game.wordLength};aspect-ratio:{$game.wordLength}/{$game
+						.guesses.length};"
+					class:playing={!won}
 				>
-					{#if won}
-						🥳 C'est gagné ! Tu as trouvé « {$game.originalAnswer} »
-					{:else}
-						🙊 Oups, perdu ! La réponse était « {$game.originalAnswer} »
-					{/if}
-				</button>
+					{#each Array.from(Array($game.guesses.length).keys()) as row (row)}
+						{@const current = row === currentIndex}
+						<h2 class="visually-hidden">Row {row + 1}</h2>
+						<div class="row" class:current>
+							{#each Array.from(Array($game.wordLength).keys()) as column (column)}
+								{@const answer = $game.answers[row]?.[column]}
+								{@const value = $game.guesses[row]?.[column] ?? ""}
+								{@const selected =
+									current && column === $game.guesses[row].length}
+								{@const exact = answer === "x"}
+								{@const close = answer === "c"}
+								{@const missing = answer === "_"}
+								<div
+									class="letter"
+									class:exact
+									class:close
+									class:missing
+									class:selected
+								>
+									{value}
+									<span class="visually-hidden">
+										{#if exact}
+											(correct)
+										{:else if close}
+											(present)
+										{:else if missing}
+											(absent)
+										{:else}
+											empty
+										{/if}
+									</span>
+									<input
+										name="guess"
+										disabled={!current}
+										type="hidden"
+										{value}
+									/>
+								</div>
+							{/each}
+						</div>
+					{/each}
+				</div>
+			</div>
+
+			{#if endRound}
+				<div class="endround-overlay">
+					<div
+						class="endround-container"
+						style="--endround-color: {won ? '#5cb85c' : '#d9534f'};"
+					>
+						<div class="endround-content">
+							{#if won}
+								<p class="smiley">🥳</p>
+								<p>C'est gagné !</p>
+								<p>Tu as trouvé <strong>« {$game.originalAnswer} »</strong></p>
+							{:else}
+								<p>🙊</p>
+								<p>Oups, tu as perdu...</p>
+								<p>
+									La réponse était <strong>« {$game.originalAnswer} »</strong>
+								</p>
+							{/if}
+
+							{#if isLastRound}
+								<hr />
+								<p>C'est fini pour aujourd'hui,</p>
+								<p>À demain !</p>
+								<p class="smiley">😘</p>
+							{:else}
+								<button on:click={game.next}>Passer au suivant</button>
+								<p style="font-size:0.6rem;">
+									(ou appuyez sur la touche entrée)
+								</p>
+							{/if}
+						</div>
+					</div>
+				</div>
 			{/if}
 		</div>
 
 		<div class="controls">
 			<div class="keyboard">
-				<button
-					on:click|preventDefault={keyclicked}
-					data-key="enter"
-					class:selected={submittable}
-					disabled={!submittable}>enter</button
-				>
-
-				<button
-					on:click|preventDefault={keyclicked}
-					data-key="backspace"
-					name="key"
-					value="backspace"
-				>
-					back
-				</button>
-
-				{#each ["azertyuiop", "qsdfghjklm", "wxcvbn"] as row}
+				{#each ["azertyuiop", "qsdfghjklm", "  wxcvbn  "] as row}
 					<div class="row">
 						{#each row as letter}
-							<button
-								on:click|preventDefault={keyclicked}
-								data-key={letter}
-								class={classnames[letter]}
-								name="key"
-								value={letter}
-								aria-label="{letter} {description[letter] || ''}"
-							>
-								{letter}
-							</button>
+							{#if letter === " "}
+								<button class="empty" disabled />
+							{:else}
+								<button
+									on:click|preventDefault={keyclicked}
+									data-key={letter}
+									class={classnames[letter]}
+									name="key"
+									value={letter}
+									aria-label="{letter} {description[letter] || ''}"
+								>
+									{letter}
+								</button>
+							{/if}
 						{/each}
 					</div>
 				{/each}
 			</div>
-		</div>
-	</form>
 
-	<div class="buttons">
-		<button on:click|preventDefault={game.next}>next</button>
+			<div class="hotkeys">
+				<button
+					on:click|preventDefault={keyclicked}
+					data-key="backspace"
+					name="key"
+					value="backspace">⌫</button
+				>
+
+				<button
+					on:click|preventDefault={keyclicked}
+					data-key="enter"
+					class:selected={submittable}
+					disabled={!submittable}>↵</button
+				>
+			</div>
+		</div>
+	</div>
+
+	<div class="game-buttons">
+		<button on:click|preventDefault={game.next}>suivant</button>
 		<button on:click|preventDefault={game.reset}>reset</button>
 	</div>
 
 	{#if won}
 		<div
-			style="position: absolute; left: 50%; top: 30%;"
+			style="position: absolute; left: 50%; top: 30%; "
 			use:confetti={{
 				force: 0.7,
 				stageWidth: window.innerWidth + 200,
-				stageHeight: window.innerHeight,
+				stageHeight: document.documentElement.scrollHeight - 250,
 				colors: ["#ff3e00", "#40b3ff", "#676778"],
 			}}
 		/>
@@ -221,19 +255,13 @@
 </main>
 
 <style>
-	/* Disable scroll bar when confettis pops */
-	/* :global(body) {
-		overflow: hidden;
-	} */
-
 	main {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		gap: 0.5rem;
-		width: 100vw;
-		height: 100vh;
-		padding: 0.5rem;
+		gap: 1.25rem;
+		width: 100%;
+		padding: 1rem;
 		box-sizing: border-box;
 	}
 
@@ -241,33 +269,37 @@
 		max-width: 320px;
 	}
 
-	.word-id {
+	.info p {
 		margin: 0;
+		text-align: center;
+		color: var(--color-text);
 	}
 
-	form {
-		flex-grow: 1;
+	/* GRID */
+	.game {
 		width: 100%;
-		height: 100%;
+		max-width: 580px;
 		display: flex;
 		flex-direction: column;
+		gap: 1.25rem;
 		align-items: center;
-		justify-content: center;
-		gap: 5px;
 	}
 
 	.grid-container {
-		flex-grow: 1;
+		position: relative;
+		width: 100%;
+	}
+
+	.grid-content {
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		width: 100%;
-		/* height: 100%; */
 	}
 
 	.grid {
-		/* aspect-ratio: ->> injected value */
-		height: 400px;
+		/* injected: aspect-ratio */
+		width: 100%;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
@@ -278,7 +310,6 @@
 		display: grid;
 		grid-template-columns: repeat(var(--row-length), 1fr);
 		gap: 0.2rem;
-		height: 100%;
 	}
 
 	.grid.playing .row.current {
@@ -286,7 +317,7 @@
 	}
 
 	.letter {
-		/* aspect-ratio: 1; */
+		aspect-ratio: 1;
 		display: flex;
 		align-items: center;
 		justify-content: center;
@@ -294,7 +325,9 @@
 		box-sizing: border-box;
 		text-transform: lowercase;
 		border: none;
-		font-size: calc(0.08 * var(--width));
+		font-size: 150%;
+		font-weight: 300;
+		font-family: var(--font-mono);
 		border-radius: 3px;
 		background: white;
 		margin: 0;
@@ -320,95 +353,129 @@
 		outline: 2px solid var(--color-theme-1);
 	}
 
-	.controls {
-		text-align: center;
+	/* END GAME OVERLAY */
+	.endround-overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		margin: 10%;
+	}
+
+	.endround-container {
+		/* injected: --endround-color  */
+		display: flex;
+		backdrop-filter: blur(0.3rem);
+		border: 3px solid var(--endround-color);
+		border-radius: 7px;
+		width: 100%;
+		height: 100%;
+		align-items: center;
 		justify-content: center;
-		height: min(18vh, 10rem);
+	}
+
+	.endround-content {
+		color: var(--color-text);
+		text-align: center;
+	}
+
+	.endround-content p {
+		line-height: 0.6;
+	}
+
+	.endround-content p.smiley {
+		font-size: 2rem;
+		line-height: 0;
+	}
+
+	.endround-content button {
+		color: var(--color-text);
+		background-color: #fff;
+		border: 1px solid var(--endround-color);
+		border-radius: 4px;
+		padding: 0.4rem 1rem;
+		margin-top: 1rem;
+		cursor: pointer;
+	}
+
+	/* CONTROLS / KEYBOARD */
+	.controls {
+		width: 100%;
+		max-width: 500px;
+		display: grid;
+		grid-template-columns: 1fr 11%;
+		gap: 0.18rem;
+		justify-content: center;
+		font-family: var(--font-mono);
+	}
+
+	.hotkeys {
+		display: flex;
+		gap: 0.18rem;
+		flex-direction: column;
+	}
+
+	.controls .hotkeys button {
+		width: 100%;
+		height: 100%;
 	}
 
 	.keyboard {
-		--gap: 0.2rem;
-		position: relative;
+		width: 100%;
 		display: flex;
 		flex-direction: column;
-		gap: var(--gap);
-		height: 100%;
+		gap: 0.18rem;
 	}
 
 	.keyboard .row {
 		display: flex;
 		justify-content: center;
-		gap: 0.2rem;
-		flex: 1;
+		gap: 0.18rem;
+	}
+
+	.controls button {
+		width: 100%;
+		background-color: white;
+		color: black;
+		border: none;
+		border-radius: 3px;
+		margin: 0;
+		cursor: pointer;
 	}
 
 	.keyboard button,
 	.keyboard button:disabled {
-		--size: min(8vw, 4vh, 40px);
-		background-color: white;
-		color: black;
-		width: var(--size);
-		border: none;
-		border-radius: 2px;
-		font-size: calc(var(--size) * 0.5);
-		margin: 0;
+		height: calc(var(--size) * 1.3);
+		aspect-ratio: 0.8;
+		font-weight: 400;
 	}
 
-	.keyboard button.exact {
+	.controls button.empty {
+		visibility: hidden;
+	}
+
+	.controls button.exact {
 		background: var(--color-theme-2);
 		color: white;
 	}
 
-	.keyboard button.missing {
+	.controls button.missing {
 		opacity: 0.5;
 	}
 
-	.keyboard button.close {
+	.controls button.close {
 		border: 2px solid var(--color-theme-2);
 	}
 
-	.keyboard button:focus {
+	.controls button:active {
 		background: var(--color-theme-1);
 		color: white;
 		outline: none;
 	}
 
-	.keyboard button[data-key="enter"],
-	.keyboard button[data-key="backspace"] {
-		position: absolute;
-		bottom: 0;
-		width: calc(1.5 * var(--size));
-		height: calc(1 / 3 * (100% - 2 * var(--gap)));
-		text-transform: uppercase;
-		font-size: calc(0.3 * var(--size));
-		padding-top: calc(0.15 * var(--size));
-	}
-
-	.keyboard button[data-key="enter"] {
-		right: calc(50% + 3.5 * var(--size) + 0.8rem);
-	}
-
-	.keyboard button[data-key="backspace"] {
-		left: calc(50% + 3.5 * var(--size) + 0.8rem);
-	}
-
-	.keyboard button[data-key="enter"]:disabled {
+	.controls button[data-key="enter"]:disabled {
 		opacity: 0.5;
-	}
-
-	.restart {
-		width: 100%;
-		padding: 1rem;
-		background: rgba(255, 255, 255, 0.5);
-		border-radius: 2px;
-		border: none;
-	}
-
-	.restart:focus,
-	.restart:hover {
-		background: var(--color-theme-1);
-		color: white;
-		outline: none;
 	}
 
 	@keyframes wiggle {
@@ -435,41 +502,26 @@
 		}
 	}
 
-	.buttons {
+	/* BUTTONS */
+	.game-buttons {
 		display: flex;
 		flex-wrap: nowrap;
 		gap: 10px;
 		align-items: center;
+		margin-top: 5rem;
 	}
 
-	.buttons button {
+	.game-buttons button {
 		background-color: transparent;
-		border: 1px solid grey;
+		border: 1px solid rgb(225, 225, 225);
 		border-radius: 5px;
 		padding: 5px 10px;
-		color: grey;
+		color: rgb(156, 156, 156);
 	}
 
-	.buttons button:hover {
+	.game-buttons button:hover {
 		cursor: pointer;
 		color: #000;
 		border-color: #000;
-	}
-
-	.end-round-panel {
-		height: 100px;
-		width: 100%;
-		flex-grow: 0;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-
-	.end-round-panel button {
-		border-color: var(--color-theme-2);
-		background-color: transparent;
-		padding: 20px 40px;
-		border-radius: 5px;
-		color: var(--color-text);
 	}
 </style>
